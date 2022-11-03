45cbf7bca7ec1217addb9c7cebe6e3d251fe8741
Ghost <miningmaster28@gmail.com>
2022-11-03 18:40:54 -0500

    Add project files.

---
+++ TitanVulkanTesting/TitanVulkanTesting.cpp
@@ -0,0 +1,12 @@
+﻿// TitanVulkanTesting.cpp : Defines the entry point for the application.
+//
+
+#include "TitanVulkanTesting.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
